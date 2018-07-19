b1d17627e070dc74265875386ac8b0fc4e355738
paully114 <34038588+paully114@users.noreply.github.com>
2018-07-19 18:47:38 +0800

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpExp = new System.Windows.Forms.DateTimePicker();
+            this.btnNow = new System.Windows.Forms.Button();
+            this.Check = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtpExp
+            // 
+            this.dtpExp.CustomFormat = "dddd, dd MMMM yyyy";
+            this.dtpExp.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpExp.Location = new System.Drawing.Point(98, 85);
+            this.dtpExp.Name = "dtpExp";
+            this.dtpExp.Size = new System.Drawing.Size(197, 20);
+            this.dtpExp.TabIndex = 0;
+            // 
+            // btnNow
+            // 
+            this.btnNow.Location = new System.Drawing.Point(98, 121);
+            this.btnNow.Name = "btnNow";
+            this.btnNow.Size = new System.Drawing.Size(75, 23);
+            this.btnNow.TabIndex = 1;
+            this.btnNow.Text = "Date Now";
+            this.btnNow.UseVisualStyleBackColor = true;
+            this.btnNow.Click += new System.EventHandler(this.btnNow_Click);
+            // 
+            // Check
+            // 
+            this.Check.Location = new System.Drawing.Point(220, 121);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(75, 23);
+            this.Check.TabIndex = 2;
+            this.Check.Text = "Check";
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.Check_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Expired Date";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 251);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Check);
+            this.Controls.Add(this.btnNow);
+            this.Controls.Add(this.dtpExp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpExp;
+        private System.Windows.Forms.Button btnNow;
+        private System.Windows.Forms.Button Check;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string now, exp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            now = DateTime.Now.ToString("dddd, dd MMMM yyyy");
+        }
+
+        private void Check_Click(object sender, EventArgs e)
+        {
+            exp = dtpExp.Value.ToString("dddd, dd MMMM yyyy");
+
+            if (now == exp)
+            {
+                MessageBox.Show("Same");
+            }
+            else
+            {
+                MessageBox.Show("Not same");
+            }
+        }
+
+        private void btnNow_Click(object sender, EventArgs e)
+        {
+            exp = dtpExp.Value.ToString("dddd, dd MMMM yyyy");
+            MessageBox.Show(now);
+            MessageBox.Show(exp);
+        }
+    }
+}
